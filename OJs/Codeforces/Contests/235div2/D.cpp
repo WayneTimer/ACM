c3a70f70881a5609c38257a289c2a48d34c24c27
Timer <linyicx@126.com>
2014-03-13 21:37:33 +0800

    update CF 235div2 D.cpp

--- OJs/Codeforces/Contests/235div2/D.cpp
+++ OJs/Codeforces/Contests/235div2/D.cpp
@@ -1,3 +1,7 @@
+/*
+设f[i][j]表示 在选取各数位状态为j，组成的数除以m余数为j的数的个数。
+则f[i][j]=sigma(f[i屏蔽第k位][(x+a[k])%m==j])
+*/
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
@@ -6,67 +10,40 @@ using namespace std;
 
 typedef long long ll;
 
-ll f[2][102][1<<18]; //f[滚动数组][余数][选取情况的状态]
-int m,n,u; //u是选取情况状态数
-int mask[270000]; //选取状态
-bool used[270000]; //表示某状态是否在之前出现过
-int b[20]; //选取第i个的所有状态在mask[]中起始位置
-int c[20]; //选取第i个的所有状态在mask[]中终止位置
-int a[20]; //记录排序后各数位
+ll s;
+ll f[1<<18][100];
+int m,n;
+int a[18];
 
 int main()
 {
-  int i,j,k,t,tmp,last;
-  int q,w;
-  char s[22];
-  memset(f,0,sizeof(f));
-  memset(mask,0,sizeof(mask));
-  memset(used,false,sizeof(used));
-  scanf("%s %d",s,&m);
-  n=strlen(s);
-  for (i=0;i<n;i++)
-    a[i]=s[i]-'0';
-  sort(a,a+n);
-  u=1;
-  b[0]=1;
-  c[0]=1;
-  q=1;
-  w=0;
-  f[0][0][0]=1;
-  for (i=1;i<=n;i++)
+  ll i;
+  int j,k,last,t;
+  scanf("%I64d%d",&s,&m);
+  n=0;
+  while (s)
   {
-    q^=w;
-    w^=q;
-    q^=w;
-    for (k=b[i-1];k<=c[i-1];k++)
-    {
-      last=-1;
-      for (j=0;j<n;j++)
-      {
-        if (a[j]==last) continue; //选取相同的数状态一样，不重复计算
-        if (i==1 && a[j]==0) continue; //首位不为0
-        if (!(mask[k] & (1<<j)))
-        {
-          int r;
-          r=mask[k] | (1<<j);
-          if (!used[r])
-          {
-            mask[++u]=r;
-            used[r]=true;
-          }
-          for (t=0;t<m;t++)
-            if (f[q][t][mask[k]])
-            {
-              tmp=(t*10+a[j])%m;
-              f[w][tmp][r]+=f[q][t][mask[k]];
-            }
-          last=a[j];
-        }
-      }
-    }
-    b[i]=c[i-1]+1;
-    c[i]=u;
+    a[n++]=s%10;
+    s/=10;
   }
-  printf("%I64d\n",f[w][0][(1<<n)-1]);
+  sort(a,a+n);
+  memset(f,0,sizeof(f));
+  f[0][0]=1;
+  for (i=0;i<1<<n;i++)
+    for (j=0;j<m;j++)
+      if (f[i][j])
+      {
+        last=-1;
+        for (k=n-1;k>=0;k--)
+          if (!(i & 1<<k) && last!=a[k])
+          {
+            if (i==0 && a[k]==0) break;
+            //反向遍历数组a,使得判前导0时这里可直接break
+            t=(j*10+a[k])%m;
+            f[i | 1<<k][t]+=f[i][j];
+            last=a[k];
+          }
+      }
+  printf("%I64d\n",f[(1<<n)-1][0]);
   return 0;
 }
